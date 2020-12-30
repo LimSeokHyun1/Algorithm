0071c301d3cd4e56ae85c6d154e569445c47b5d5
LimSeokHyun1 <72636327+LimSeokHyun1@users.noreply.github.com>
2020-12-30 16:17:29 +0900

    Update <2661>GoodSequence.cpp

--- <2661>GoodSequence.cpp
+++ <2661>GoodSequence.cpp
@@ -6,35 +6,29 @@ using namespace std;
 int good_array[80];
 int array_length, recursive_end_flag = 0;
 
-void Show_Array();
 void Make_Array(int tar_idx, int pre);
-bool Deter_Good_Array();
+bool Deter_Good_Array(int tar_idx);
 
 int main(){
 	cin >> array_length;
 	Make_Array(0, -1);
 }
 
-void Show_Array(){
-	for(int i = 0; i < array_length; i++){
-		cout << good_array[i];
-	}
-	cout << "\n";
-}
-
 void Make_Array(int tar_idx, int pre){
-	if(tar_idx == array_length){
-		if(array_length <= 3){
-			recursive_end_flag = 1;
-			Show_Array();
-			return;
-		}
-		if(Deter_Good_Array()){
-			recursive_end_flag = 1;
-			Show_Array();
-		}
+	if(array_length <= 3 && tar_idx == array_length){
+		Show_Array();
+		recursive_end_flag = 1;
 		return;
 	}
+	
+	if(Deter_Good_Array(tar_idx)){
+		if(tar_idx == array_length){
+			Show_Array();
+			recursive_end_flag = 1;
+			return;
+		}
+	}
+	else	return;
 	
 	for(int i = 1; i <= 3; i++){
 		if(i == pre) continue;
@@ -44,18 +38,15 @@ void Make_Array(int tar_idx, int pre){
 	}
 }
 
-bool Deter_Good_Array(){
-	Show_Array();
+bool Deter_Good_Array(int cur_length){
 	int pre_idx, cur_idx, same_num_flag;
-	for(int i = 2; i <= array_length / 2; i++){
+	for(int i = 2; i <= cur_length / 2; i++){
 		pre_idx = 0;
 		cur_idx = 0;
-		while(cur_idx + i <= array_length){
+		while(cur_idx + i <= cur_length){
 			same_num_flag = 0;
 			cur_idx = pre_idx + i;
-			//cout << "cur_idx: " << cur_idx << " pre_idx: " << pre_idx << endl;
 			for(int j = 0; j < i; j++){
-				//cout << "cur: " << good_array[cur_idx + j] << " pre: " << good_array[pre_idx + j] << endl;
 				if(good_array[cur_idx + j] != good_array[pre_idx + j]){
 					same_num_flag = 1;
 					break;
