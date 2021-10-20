80f25b0721d7703cfdd470691e1cd1c053fb7d01
LimSeokHyun1 <ish990730@naver.com>
2021-10-21 03:40:47 +0900

    <2750>

--- Python/FindNumber.py
+++ Python/FindNumber.py
@@ -1,22 +1,8 @@
-import bisect
-
 N = int(input())
-numset1 = input().split()
-M = int(input())
-numset2 = input().split()
-length = len(numset1) - 1
-numset1.sort()
+numbers = []
+for i in range(N):
+    numbers.append(int(input()))
 
-def binary_search(first, last ,tar):
-    mid = (first + last) // 2
-    if first > last:
-        return '0'
-    if numset1[mid] == tar:
-        return '1'
-    elif numset1[mid] < tar:
-        return binary_search(mid + 1, last, tar)
-    else:
-        return binary_search(first, mid - 1, tar)
-
-for i in numset2:
-    print(binary_search(0, length, i))
+numbers.sort()
+for i in range(N):
+    print(numbers[i])
