943def4b172bb0ba9f2bc76ddadc0b72117ab0c4
LimSeokHyun1 <ish990730@naver.com>
2021-10-21 03:51:50 +0900

    Who are you

--- Python/FindNumber.py
+++ Python/FindNumber.py
@@ -1,13 +1,22 @@
-N = int(input())
-numbers = []
-for i in range(N):
-    numbers.append(int(input()))
+import bisect
 
-<<<<<<< HEAD
-numbers.sort()
-for i in range(N):
-    print(numbers[i])
-=======
+N = int(input())
+numset1 = input().split()
+M = int(input())
+numset2 = input().split()
+length = len(numset1) - 1 # 4
+numset1.sort()
+
+def binary_search(first, last ,tar):
+    mid = (first + last) // 2
+    if first > last:
+        return '0'
+    if numset1[mid] == tar:
+        return '1'
+    elif numset1[mid] < tar:
+        return binary_search(mid + 1, last, tar)
+    else:
+        return binary_search(first, mid - 1, tar)
+
 for i in numset2:
     print(binary_search(0, length, i))
->>>>>>> bd9dc6f0f51629a2dc4113fe5fd5b699e341183b
--- Python/SortNumber.py
+++ Python/SortNumber.py
@@ -1,1 +1,8 @@
-print("Please !")
+N = int(input())
+numbers = []
+for i in range(N):
+    numbers.append(int(input()))
+
+numbers.sort()
+for i in range(N):
+    print(numbers[i])
