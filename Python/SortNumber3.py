1b4ecb5ce1772af95ed21651b10bf93d6bb7f55b
LimSeokHyun1 <72636327+LimSeokHyun1@users.noreply.github.com>
2021-10-23 23:53:36 +0900

    <10989>

--- Python/SortNumber3.py
+++ Python/SortNumber3.py
@@ -1,1 +1,11 @@
-print("HELLO")
+import sys
+
+N = int(input())
+numbers = [0] * 10000
+for i in range(N):
+    numbers[int(sys.stdin.readline()) - 1] += 1
+
+for i in range(10000):
+    for j in range(numbers[i]):
+        print(i + 1)
+
