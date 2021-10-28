849db05af4d9276889acfc2259654616646a9f46
LimSeokHyun1 <72636327+LimSeokHyun1@users.noreply.github.com>
2021-10-28 21:51:48 +0900

    <1026>

--- Python/Treasure.py
+++ Python/Treasure.py
@@ -15,3 +15,29 @@ for i in range(N):
     result += int(listA[i]) * int(listB[i])
 
 print(result)
+
+================================================================
+
+# B는 재배열 불가 조건 추가 + greedy algorithm
+import sys
+
+N = int(input())
+result = 0
+
+listA = list(map(int, sys.stdin.readline().split()))
+listB = list(map(int, sys.stdin.readline().split()))
+visited = [0] * N
+
+listA.sort()
+
+for i in range(N):
+    maximum = 0
+    max_idx = 0
+    for j in range(N):
+        if maximum < listB[j] and visited[j] == 0:
+            maximum = listB[j]
+            max_idx = j
+    visited[max_idx] = 1
+    result += maximum * listA[i]
+
+print(result)
